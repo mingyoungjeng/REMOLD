20f767c26e877698587cf999125d76d81dcb9739
Joshua Jeng <mingyoungjeng@gmail.com>
2018-12-06 14:50:57 -0600

    make Brain a button

--- REMOLD/HomeViewController.swift
+++ REMOLD/HomeViewController.swift
@@ -17,7 +17,6 @@ class HomeViewController: UIViewController {
      
         // Do any additional setup after loading the view.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
